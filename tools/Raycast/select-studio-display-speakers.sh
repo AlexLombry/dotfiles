2be712aab724632aa6011784493c8ca56d215e65
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2023-09-09 17:19:06 +0200

    Sound selector for Raycast

---
+++ tools/Raycast/select-studio-display-speakers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Select Studio Display Speakers
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Alex Lombry
+
+SwitchAudioSource -s "Studio Display Speakers"
