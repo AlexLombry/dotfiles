e0a19666be44e7de7c059e03078ff5215b03440b
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2023-12-12 18:51:28 +0100

    update advent of code starter script

--- tools/Raycast/advent-of-code-starter.sh
+++ tools/Raycast/advent-of-code-starter.sh
@@ -11,12 +11,21 @@
 # Documentation:
 # @raycast.author Alex Lombry
 
+FOLDER=$HOME/Code/AdventOfCode
+DAY=$(date +'%d')
+YEAR=$(date +%Y)
+
 if [ $(date +%m) != 12 ]; then
   echo "It's not December yet"
   exit 1
 fi
 
 cd $HOME/Code/AdventOfCode
+if [ -f $FOLDER/src/aoc$YEAR/Day$DAY.kt ]; then
+  echo "$FOLDER/src/aoc$YEAR/Day$DAY.kt already exist"
+  exit 1
+fi
+
 ./day.sh
 
 echo "Have fun for this AdventOfCode session"
