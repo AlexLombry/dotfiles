bf4a2407a918d2c535bb71ca43e28cd3e2344a7e
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2024-03-20 22:37:16 +0100

    feat: Update plugins

--- nvimCustom/custom/init.lua
+++ nvimCustom/custom/init.lua
@@ -3,8 +3,24 @@
 -- settings for Markdown
 local opt = vim.opt
 
+opt.number = true
 opt.relativenumber = true
 
+opt.swapfile = false
+
+opt.smartindent = true
+
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.wrap = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+opt.undofile = true
+
+opt.incsearch = true
 -- Auto resize panes when resizing nvim window
 --autocmd("VimResized", {
 --   pattern = "*",
