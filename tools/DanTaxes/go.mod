3287d488cf3f8d817d5c18325b78a48e9415964d
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2024-04-12 21:57:57 +0200

    feat: Go code insides of dotfiles

---
+++ tools/DanTaxes/go.mod
@@ -0,0 +1,5 @@
+module DanTaxes/v2
+
+go 1.22.2
+
+require github.com/otiai10/gosseract/v2 v2.4.1
--- tools/DanTaxes/main.go
+++ tools/DanTaxes/main.go
@@ -5,6 +5,7 @@ import (
 	"io/fs"
 	"log"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/otiai10/gosseract/v2"
@@ -16,10 +17,13 @@ func main() {
 
 	folderPath := "./files"
 
+	numberPattern := regexp.MustCompile(`[0-9]+[ ,]*[0-9]*\.[0-9]+`)
+
 	err := filepath.Walk(folderPath, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+
 		if info.IsDir() {
 			return nil
 		}
@@ -39,7 +43,10 @@ func main() {
 
 		for _, line := range lines {
 			if strings.Contains(line, "Mont") {
-				fmt.Println(line)
+				matches := numberPattern.FindAllString(line, -1)
+				for _, match := range matches {
+					fmt.Println(match)
+				}
 			}
 		}
 		return nil
