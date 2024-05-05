46289101d14421980abc70935122f1314cd75926
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2024-05-05 18:39:13 +0200

    update tmux

--- zsh/alex/functions.zsh
+++ zsh/alex/functions.zsh
@@ -465,8 +465,8 @@ function openSession () {
     tmux send-keys -t $SESSION:2.1 "G" Enter
     tmux send-keys -t $SESSION:2.3 "cd $WORK_DIR && ll" Enter
 
-    WORK_DIR="$HOME/ManoMano/"
-    tmux new-window -t $SESSION:3 -n 'ManoMano Home'
+    WORK_DIR="/Volumes/ManoMano"
+    tmux new-window -t $SESSION:3 -n 'ManoMano Home Directory'
     tmux split-window -h -t $SESSION:3.1
     tmux split-window -v -t $SESSION:3.2
     tmux send-keys -t $SESSION:3.1 "cd $WORK_DIR && vim notes" Enter
@@ -481,15 +481,16 @@ function openSession () {
 function tm() {
     SESSION="StarK"
 
+    WORK_DIR="/Volumes/ManoMano"
     tmux kill-session -t $SESSION
     tmux new-session -d -s $SESSION -n 'Dotfiles'
     tmux send-keys -t $SESSION:1 "cd $HOME/dotfiles && ll" Enter
 
     tmux new-window -t $SESSION:2 -n 'ManoMano'
-    tmux send-keys -t $SESSION:2 "cd $HOME/ManoMano && ll" Enter
+    tmux send-keys -t $SESSION:2 "cd $WORK_DIR && ll" Enter
 
     tmux new-window -t $SESSION:3 -n 'Dev Workspace'
-    tmux send-keys -t $SESSION:3 "cd $HOME/ManoMano/gradle-dev-workspace/web && ll" Enter
+    tmux send-keys -t $SESSION:3 "cd $WORK_DIR/gradle-dev-workspace/web && ll" Enter
 
     tmux new-window -t $SESSION:4 -n 'Custom'
     tmux send-keys -t $SESSION:4 "cd $HOME/Desktop && ll" Enter
