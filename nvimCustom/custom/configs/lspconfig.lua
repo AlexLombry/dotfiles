ccc03870b9f4d93b0ad228d772833e74675ea6bc
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2025-04-08 21:17:14 +0200

    fix: From tsserver to ts

--- nvimCustom/custom/configs/lspconfig.lua
+++ nvimCustom/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "kotlin_language_server" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "kotlin_language_server" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
