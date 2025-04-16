9393c250b6b973e9512bc1aa2c1a8403da4fa96a
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2025-04-16 11:15:48 +0200

    Remove atom for kymsu

--- tools/kymsupdate/plugins.d/atom.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "⚛️ -- Atom editor will be shiny when you'll be back from your coffee/tea break!"
-if hash apm-beta 2>/dev/null; then
-    apm-beta upgrade -c false
-fi
-if hash apm 2>/dev/null; then
-    apm upgrade -c false
-fi
-echo ""
