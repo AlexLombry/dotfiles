7a864bd97ae7f8390d4a895dd5beb19fa19275d6
Alex Lombry <1451210+AlexLombry@users.noreply.github.com>
2026-06-02 00:24:38 +0200

    feat(nvim): modernise plugin stack and document keymaps
    - Replace nvim-cmp with blink.cmp (Rust-based, faster completion)
    - Replace neodev.nvim with lazydev.nvim (officially deprecated by folke)
    - Consolidate alpha, lazygit, indent-blankline into snacks.nvim
    - Use vim.diagnostic.config for sign column instead of deprecated vim.fn.sign_define
    - Fix deprecated vim.loop → vim.uv in lazy bootstrap
    - Add docs/vim.md with full keymap reference

--- stow/zsh/.oh-my-zsh/custom/alex/git.zsh
+++ stow/zsh/.oh-my-zsh/custom/alex/git.zsh
@@ -3,6 +3,7 @@ alias nah="git reset --hard; git clean -df;"
 alias wip='git status && git add . && git commit -m "Work In Progress"'
 alias gpra='git pull --rebase --autostash'
 alias glfh="git log --pretty=format: --name-only | sort | uniq -c | sort -rg | head -10"
+alias amend='git commit --verbose --amend'
 
 # Open a github/gitlab repo in the browser
 hb() {
